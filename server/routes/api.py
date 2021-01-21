e27987b3c38c3a77b5d6918e88fb970de22a40fe
Vincenzo Saturnino <saturnino.vincenzo@it.ibm.com>
2021-01-21 14:02:09 +0100

    log su file

--- server/routes/api.py
+++ server/routes/api.py
@@ -9,6 +9,15 @@ from flask import request
 
 # resource_path = "/resources"
 
+#RESOURCES FOLDER
+#discriminiamo il path in base alla macchina su cui gira l'applicativo
+if os.uname()[4] == 'ppc64le':
+    resource_path = "/resources"
+else:
+    resource_path = '/Users/it058917/work/ENI/Progetti/git/annotatori/caption/CD-annotator/resources'
+#############
+
+
 # #LOGGING
 # LOG_FILENAME = os.path.join(resource_path, 'logging.log')
 # my_logger = logging.getLogger('MyLogger')
@@ -21,12 +30,25 @@ from flask import request
 # my_logger.addHandler(handler)
 ############
 
-
+#LOGGING
+LOG_FILENAME = os.path.join(resource_path, 'logging.log')
+#LOG_FILENAME = 'logging.log'
+my_logger = logging.getLogger('MyLogger')
+#my_logger.setLevel(level_logging)
+my_logger.setLevel("INFO")
+# create formatter
+formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(module)s;%(message)s","%Y-%m-%d %H:%M:%S")
+# Add the log message handler to the logger
+handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=10000, backupCount=5)
+handler.setFormatter(formatter)
+my_logger.addHandler(handler)
+############
 
 @app.route("/check")
 def check():
     """health route"""
     user = request.args.get('name')
     state = {"ciao":user }
+    my_logger.info(state)
     #my_logger.info("Function called by " + username)
     return jsonify(state)
